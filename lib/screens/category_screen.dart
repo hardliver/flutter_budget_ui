9e9b1b5ec4fe84d13e2e498c333bfca953e10aaa
hammerandnail <yelionel26@gmail.com>
2021-01-23 10:40:10 +0800

    21. Custom Painter - Radial Progress Bar

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_budget_ui/helpers/color_helpers.dart';
+import 'package:flutter_budget_ui/models/expense_model.dart';
+import 'package:flutter_budget_ui/widgets/radial_painter.dart';
 
 import '../models/category_model.dart';
 
@@ -14,6 +17,13 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
+    double totalAmountSpent = 0;
+    widget.category.expenses.forEach((Expense expense) {
+      totalAmountSpent += expense.cost;
+    });
+    final double amountLeft = widget.category.maxAmount - totalAmountSpent;
+    final double percent = amountLeft / widget.category.maxAmount;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.category.name),
@@ -43,6 +53,23 @@ class _CategoryScreenState extends State<CategoryScreen> {
                     blurRadius: 6.0,
                   )
                 ],
+              ),
+              child: CustomPaint(
+                foregroundPainter: RadialPainter(
+                  bgColor: Colors.grey[200],
+                  lineColor: getColor(context, percent),
+                  percent: percent,
+                  width: 15.0
+                ),
+                child: Center(
+                  child: Text(
+                    '\$${amountLeft.toStringAsFixed(2)} / \$${widget.category.maxAmount}',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
               ),
             )
           ],

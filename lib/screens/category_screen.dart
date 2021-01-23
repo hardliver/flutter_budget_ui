f9b4b161cb8446d89cb5564cb6f1f5a58d3fa818
hammerandnail <yelionel26@gmail.com>
2021-01-23 10:59:39 +0800

    22. Expense List

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_budget_ui/helpers/color_helpers.dart';
 import 'package:flutter_budget_ui/models/expense_model.dart';
@@ -15,6 +17,52 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  _buildExpenses() {
+    List<Widget> expenseList = [];
+    widget.category.expenses.forEach((Expense expense) {
+      expenseList.add(Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        height: 80.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Row(
+            children: <Widget>[
+              Text(
+                expense.name,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '-\$${expense.cost.toStringAsFixed(2)}',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              )
+            ],
+          ),
+        ),
+      ));
+    });
+    return Column(children: expenseList);
+  }
+
   @override
   Widget build(BuildContext context) {
     double totalAmountSpent = 0;
@@ -56,11 +104,10 @@ class _CategoryScreenState extends State<CategoryScreen> {
               ),
               child: CustomPaint(
                 foregroundPainter: RadialPainter(
-                  bgColor: Colors.grey[200],
-                  lineColor: getColor(context, percent),
-                  percent: percent,
-                  width: 15.0
-                ),
+                    bgColor: Colors.grey[200],
+                    lineColor: getColor(context, percent),
+                    percent: percent,
+                    width: 15.0),
                 child: Center(
                   child: Text(
                     '\$${amountLeft.toStringAsFixed(2)} / \$${widget.category.maxAmount}',
@@ -71,7 +118,8 @@ class _CategoryScreenState extends State<CategoryScreen> {
                   ),
                 ),
               ),
-            )
+            ),
+            _buildExpenses(),
           ],
         ),
       ),
